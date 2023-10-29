093bf3ee4c22b23ac03522a9da05a191378950cd
唐隆辉 <1776234240@qq.com>
2023-10-29 23:10:18 +0800

    base

---
+++ 02_base/literal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+using std::cin;
+
+const int SIZE = 5;
+int main() {
+    system("chcp 65001");
+    int a=-1;
+    bool b =a;
+    std::cout << "hello " << std::endl;
+    cin >> a;
+    std::cout << "你好" << std::endl;
+
+    printf("%d",b);
+    return 0;
+}
